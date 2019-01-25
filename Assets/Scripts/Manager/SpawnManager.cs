683065dbcaa6d32dfc6252b953246ee7e051e516
Pierre <pierre.zanettacci@hotmail.fr>
2019-01-26 00:48:38 +0100

    Spawn des "ennemis"

---
+++ Assets/Scripts/Manager/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : SingletonBehaviour<SpawnManager> {
+
+    
+
+    [SerializeField]
+    private GameObject[] Enemies;
+    
+
+    [SerializeField]
+    private Transform[] positions;
+
+    public float minimum;
+    public float maximum;
+    private float apparition; 
+    private float temps;
+    private int RandomSpawn;
+    private int RandomEnemy;
+
+
+	void Start () {
+        Spawn();
+	}
+	
+	
+	void Update () {
+        
+	}
+
+    void Spawn()
+    {
+        float randomTime = Random.Range(minimum, maximum);
+        Invoke("Spawn", randomTime);
+        RandomSpawn = Random.Range(0, positions.Length);
+        RandomEnemy = Random.Range(0, Enemies.Length);
+        Debug.Log(RandomEnemy);
+        Instantiate(Enemies[RandomEnemy], positions[RandomSpawn].position, positions[RandomSpawn].rotation); 
+    }
+}
