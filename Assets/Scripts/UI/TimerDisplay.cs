ea5d7813d75f3a0d7416ed2284d9d332d4350c2e
Pierre <pierre.zanettacci@hotmail.fr>
2019-01-26 17:47:12 +0100

    Changement du script de Timer

--- Assets/Scripts/UI/TimerDisplay.cs
+++ Assets/Scripts/UI/TimerDisplay.cs
@@ -8,43 +8,32 @@ public class TimerDisplay : MonoBehaviour
 
     // Use this for initialization
 
-    private float timer = 1;
-    public Text TimerText;
-    public GameObject Stop_Cleaning;
-    public GameObject End_Screen;
-    public Text EndScore;
+    private float timer = 10;
+    public Text timerText;
+    
+    public Color colorEndGame;
 
     void Start()
     {
-        Stop_Cleaning.SetActive(false);
-        End_Screen.SetActive(false);
-        EndScore.GetComponent<Text>().enabled = false;
-        EndScore.text = "0";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimerText.text = Mathf.RoundToInt(timer).ToString();
+        
+        timerText.text = Mathf.RoundToInt(timer).ToString();
         if (timer <= 10)
         {
-            TimerText.color = Color.red;
+            timerText.color = colorEndGame;
         }
-
-        if (timer >= 0)
-        {
-            timer -= Time.deltaTime;
-        }
-        else
-        {
-            Stop_Cleaning.SetActive(true);
-            Invoke("SpawnEndScreen", 1);
-        }
+        
+       
     }
 
-    void SpawnEndScreen()
+    /*void SpawnEndScreen()
     {
         End_Screen.SetActive(true);
         EndScore.GetComponent<Text>().enabled = true;
-    }
+    }*/
 }
