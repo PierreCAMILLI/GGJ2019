a12c1a904820ec6fee7f32ce322d2e9306018d37
Pierre <pierre.zanettacci@hotmail.fr>
2019-01-27 10:26:37 +0100

    Rajout sprite tuto  + corrections spawn

---
+++ Assets/Scripts/Interactables/SpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public bool available = true;
+
+    public void setAvailable(bool available)
+    {
+        this.available = available;
+    }
+}
